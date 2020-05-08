5a619f90cf4b8811cb6e6d644784b7fb85c23f73
Danish Jafri <danishjafri@outlook.com>
2020-05-08 06:10:48 +0100

    Added generic repository.

---
+++ BookStore.Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Repositories
+{
+    public interface IGenericRepository<T> : IDisposable where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetById(int id);
+
+        Task<T> Add(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(int id);
+    }
+}
